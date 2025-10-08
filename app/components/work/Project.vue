dd17db3e346c2ede067639b56e12aeaa7cddd9e8
xunita <yayasopeguesoro@gmail.com>
2025-10-08 20:03:42 +0200

    update youtube iframe

--- app/components/work/Project.vue
+++ app/components/work/Project.vue
@@ -40,22 +40,9 @@ const openLink = () => {
         >{{ $t("viewProject") }}</UButton
       >
     </div>
-    <iframe
-      v-if="work.iframe"
-      :src="
-        work.iframe_link ??
-        'https://www.youtube-nocookie.com/embed/0gmXsyyDDnU?si=pDSzchUBDKb2NQu7'
-      "
-      class="rounded-lg border-2 border-white-700 cursor-pointer max-w-[624px] max-h-[351px] w-full h-full"
-      title="YouTube video player"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      referrerpolicy="strict-origin-when-cross-origin"
-      allowfullscreen
-      style="aspect-ratio: 16/9; width: 100%"
-    ></iframe>
+
     <NuxtImg
-      v-else
+      v-if="!work.iframe"
       target="_blank"
       :src="work.img ?? '/images/notfound.png'"
       :alt="$t(`${work.id}.title`)"
@@ -66,5 +53,19 @@ const openLink = () => {
       height="351"
       @click="openLink"
     />
+    <iframe
+      v-else
+      :src="
+        work.iframe_link ??
+        'https://www.youtube-nocookie.com/embed/0gmXsyyDDnU?si=pDSzchUBDKb2NQu7'
+      "
+      class="rounded-lg border-2 border-white-700 cursor-pointer max-w-[624px] max-h-[351px] w-full h-full"
+      :title="work.title ?? 'Youtube video'"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerpolicy="strict-origin-when-cross-origin"
+      allowfullscreen
+      style="aspect-ratio: 16/9; width: 100%"
+    ></iframe>
   </div>
 </template>
