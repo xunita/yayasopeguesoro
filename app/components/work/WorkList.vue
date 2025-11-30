ebd3124b94e0e78deed4094197554ebdbe2b4175
Yaya Sopegue Soro <42295537+xunita@users.noreply.github.com>
2025-11-30 23:36:19 +0100

    Adjust padding in WorkList component
    Remove top padding from WorkList component.

--- app/components/work/WorkList.vue
+++ app/components/work/WorkList.vue
@@ -172,7 +172,7 @@ watch(locale, (newLocale) => {
       :key="'project-' + work.id"
       v-motion-slide-visible-once-bottom
       :work="work"
-      class="self-center w-full max-w-[1080px] md:px-20 px-10 pt-10"
+      class="self-center w-full max-w-[1080px] md:px-20 px-10"
       :class="{
         'pb-10': work.id !== 'pincasse'
       }"
