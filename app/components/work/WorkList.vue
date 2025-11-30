bb1cecd77c01e9e9ef67c9fd15a8cbb7712380b5
Yaya Sopegue Soro <42295537+xunita@users.noreply.github.com>
2025-11-30 23:24:53 +0100

    Refactor WorkList.vue class attributes

--- app/components/work/WorkList.vue
+++ app/components/work/WorkList.vue
@@ -172,7 +172,7 @@ watch(locale, (newLocale) => {
       :key="'project-' + work.id"
       v-motion-slide-visible-once-bottom
       :work="work"
-      class="self-center min-md:w-5/6 min-w-full max-w-[1080px] w-full md:px-0 px-10 pt-10"
+      class="self-center md:w-5/6 min-w-full px-10 pt-10"
       :class="{
         'border-t border-white': work.id !== 'salyeo',
         'pb-10': work.id !== 'pincasse'
