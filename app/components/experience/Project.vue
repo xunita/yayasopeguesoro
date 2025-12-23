a7c74f6e9dc1a7a79a84967f1f3b73e61e427e4f
Yaya Sopegue Soro <42295537+xunita@users.noreply.github.com>
2025-12-23 14:55:53 +0100

    Update Project.vue

--- app/components/experience/Project.vue
+++ app/components/experience/Project.vue
@@ -7,9 +7,9 @@ const props = defineProps({
 });
 </script>
 <template>
-  <div class="flex flex-col space-y-2 text-sm">
+  <div class="flex flex-col space-y-2">
     <div class="flex flex-col gap-1">
-      <span class="text-muted">
+      <span class="text-muted text-sm">
         {{ projet.date ?? "??" }}
       </span>
       <span class="font-semibold">
