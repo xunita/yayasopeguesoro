59042d800eed61449e228e48fcc52091a6df25e1
Yaya Sopegue Soro <42295537+xunita@users.noreply.github.com>
2025-12-23 14:43:09 +0100

    Refactor class attributes in Project.vue

--- app/components/experience/Project.vue
+++ app/components/experience/Project.vue
@@ -21,9 +21,9 @@ const props = defineProps({
       <div
         v-for="stack in projet.stacks"
         :key="'stack-' + stack.id"
-        class="flex gap-1 items-center"
+        class="flex gap-1"
       >
-        <UIcon name="heroicons-solid:plus-sm" class="size-6" />
+        <UIcon name="heroicons-solid:plus-sm" class="size-6 shrink-0 w-fit" />
         <span> {{ stack }}</span>
       </div>
     </div>
